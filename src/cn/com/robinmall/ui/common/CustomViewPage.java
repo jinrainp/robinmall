c2a15d4d51f52c834a99be952d8d14d9c5beffde
jinrain <jinrainp@gmail.com>
2014-03-31 16:20:44 +0800

    new to add

---
+++ src/cn/com/robinmall/ui/common/CustomViewPage.java
@@ -0,0 +1,13 @@
+package cn.com.robinmall.ui.common;
+
+import android.content.Context;
+import android.widget.RelativeLayout;
+
+public class CustomViewPage extends RelativeLayout{
+
+	public CustomViewPage(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+}
